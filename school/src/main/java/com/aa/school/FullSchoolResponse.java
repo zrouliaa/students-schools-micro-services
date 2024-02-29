f6da3c76391ce3e83ffd129247a3c015bc332869
zrouliaa <zrouli.aa@gmail.com>
2024-02-29 17:54:15 +0000

    First commit

---
+++ school/src/main/java/com/aa/school/FullSchoolResponse.java
@@ -0,0 +1,16 @@
+package com.aa.school;
+
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FullSchoolResponse {
+
+    private String name;
+    private String email;
+    List<Student> students;
+}
---
+++ school/src/main/java/com/aa/school/SchoolRepository.java
@@ -0,0 +1,6 @@
+package com.aa.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+}
